7f705647855a7973f6d6ee4596a65b1667d08004
Pablo Herrero Gómez <99969239+phg18@users.noreply.github.com>
2025-05-21 10:40:25 +0200

    Update setup_practice.sh

--- setup_practice.sh
+++ setup_practice.sh
@@ -7,8 +7,8 @@ mkdir -p /home/docker/catkin_ws/src/robotica_inteligente/scripts
 mv *.py /home/docker/catkin_ws/src/robotica_inteligente/scripts/
 
 # Move launch files to the launch folder
-mkdir -p /home/phg18/Intelligent_robotics_practice/src/robotica_inteligente/launch
-mv *.launch /home/phg18/Intelligent_robotics_practice/src/robotica_inteligente/launch/
+mkdir -p /home/docker/catkin_ws/src/robotica_inteligente/launch
+mv *.launch /home/docker/catkin_ws/src/robotica_inteligente/launch/
 
 echo "Files have been organized successfully."
 echo "Now you can run: catkin_make and source devel/setup.bash"
